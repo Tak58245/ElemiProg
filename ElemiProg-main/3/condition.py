df4b8f1f90c8765aa791837d32e01b4797717d29
neo582455 <62070433+neo582455@users.noreply.github.com>
2021-09-24 15:23:13 +0200

    Test

---
+++ ElemiProg-main/3/condition.py
@@ -0,0 +1,60 @@
+number=10
+if number < 10:
+    print("1.true")
+else:
+    print("1.false")
+
+if number < 100:
+    print("2.true")
+else:
+    print("2.false")
+
+if number %2==0:
+    print("3.true")
+else:
+    print("3.false")
+
+oszto=5
+if number % oszto ==0:
+    print("4.true osztoja numbernek")
+else:
+    if oszto % number == 0:
+        print("4.true number osztoja")
+    else:
+        print("4.false")
+
+
+str1="Ez egy teszt szoveg"
+str2="Ez a masik teszt szovege"
+
+if str1.startswith(str2[0]):
+    print("5.A ket szoveg elso karaktere megegyezik")
+else:
+    print("5.A ket szoveg elso karaktere nem egyezik meg")
+
+if str1.endswith(str2[-1]):
+    print("5.A ket szoveg utolso karaktere megegyezik")
+else:
+    print("5.A ket szoveg utolso karaktere nem egyezik meg")
+
+if number != 0:
+    if number >= 1:
+        print("6.A szam pozitiv")
+    else:
+        print(".A szam negativ")
+else:
+    print("6.A szam 0")
+
+if str1[0].isupper():
+    print("7.true")
+else:
+    print("7.false")
+
+
+
+
+
+
+
+
+
